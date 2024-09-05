c242a49f3fa57231bcae0d16647faa71dc724e59
Erfan Riazati <erf.riazati@gmail.com>
2024-09-05 20:44:30 +0330

    📦️ make : catkin package build and update
    - sensor_msgs
    - nav_msgs

--- build/lenna_description/catkin_generated/installspace/lenna_descriptionConfig.cmake
+++ build/lenna_description/catkin_generated/installspace/lenna_descriptionConfig.cmake
@@ -185,7 +185,7 @@ foreach(t ${lenna_description_EXPORTED_TARGETS})
   endif()
 endforeach()
 
-set(depends "geometry_msgs;roscpp;rospy;std_msgs")
+set(depends "geometry_msgs;roscpp;rospy;sensor_msgs;nav_msgs;std_msgs")
 foreach(depend ${depends})
   string(REPLACE " " ";" depend_list ${depend})
   # the package name of the dependency must be kept in a unique variable so that it is not overwritten in recursive calls
--- build/lenna_description/catkin_generated/pkg.develspace.context.pc.py
+++ build/lenna_description/catkin_generated/pkg.develspace.context.pc.py
@@ -1,7 +1,7 @@
 # generated from catkin/cmake/template/pkg.context.pc.in
 CATKIN_PACKAGE_PREFIX = ""
 PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "geometry_msgs;roscpp;rospy;std_msgs".replace(';', ' ')
+PROJECT_CATKIN_DEPENDS = "geometry_msgs;roscpp;rospy;sensor_msgs;nav_msgs;std_msgs".replace(';', ' ')
 PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
 PROJECT_NAME = "lenna_description"
 PROJECT_SPACE_DIR = "/home/erfan/Repositories/lmr-simulator/devel/.private/lenna_description"
